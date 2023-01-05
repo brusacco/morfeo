40211830d5e6fbf886ad8e4347289b4fb396acac
Bruno Sacco <brusacco@gmail.com>
2023-01-05 14:20:33 -0300

    changes in popular entries

--- app/controllers/entry_controller.rb
+++ app/controllers/entry_controller.rb
@@ -4,7 +4,7 @@ class EntryController < ApplicationController
   def show; end
 
   def popular
-    @entries = Entry.has_interactions.a_day_ago.where.not(image_url: nil).order(total_count: :desc)
+    @entries = Entry.joins(:site).has_interactions.a_day_ago.where.not(image_url: nil).order(total_count: :desc)
     @tags = @entries.tag_counts_on(:tags).order('count desc')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   get 'tag/show'
   get 'entry/show'
-  get 'entry/popular'
+  get 'entry/popular', as: 'popular_entries'
   get '/site/:id', to: 'site#show', as: 'site'
   get '/tag/:id', to: 'tag#show', as: 'tag'
   devise_for :admin_users, ActiveAdmin::Devise.config
