5ca9173b6311090b522f40285f4e6db649b39a34
Bruno Sacco <brusacco@gmail.com>
2023-01-11 02:52:30 -0300

    site controller changes

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,7 +4,7 @@ class SiteController < ApplicationController
   def show
     @site = Site.find(params[:id])
     @entries_stats = @site.entries.a_month_ago.group_by_day(:published_at)
-    @entries = @site.entries.has_image.order(published_at: :desc).limit(250)
+    @entries = @site.entries.has_interactions.has_image.order(published_at: :desc).limit(250)
     @tags = @entries.tag_counts_on(:tags).order('count desc').limit(20)
   end
 end
