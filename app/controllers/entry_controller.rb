96c8b051f6403b14a205d89a826c1fcb40732206
Bruno Sacco <brusacco@gmail.com>
2023-01-28 16:51:38 -0300

    added additional graphs

--- app/controllers/entry_controller.rb
+++ app/controllers/entry_controller.rb
@@ -6,5 +6,26 @@ class EntryController < ApplicationController
   def popular
     @entries = Entry.joins(:site).has_interactions.a_day_ago.where.not(image_url: nil).order(total_count: :desc)
     @tags = @entries.tag_counts_on(:tags).order('count desc')
+
+    @tags.each do |tag|
+      @entries.each do |entry|
+        tag.interactions ||= 0
+        tag.interactions += entry.total_count
+      end
+    end
+
+    @tags_count = {}
+    @tags.each { |n| @tags_count[n.name] = n.count }
+
+    @tags_interactions = {}
+    @entries.each do |entry|
+      entry.tags.each do |tag|
+        @tags_interactions[tag.name] ||= 0
+        @tags_interactions[tag.name] += entry.total_count
+      end
+    end
+
+    @tags_interactions = @tags_interactions.sort_by { |_k, v| v }
+    @tags_interactions.reverse
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,27 @@ class HomeController < ApplicationController
     @sites = Site.where(total_count: 1..).order(total_count: :desc)
     @entries = Entry.has_interactions.has_image.includes(:site).order(published_at: :desc).limit(300)
     @tags = @entries.tag_counts_on(:tags).order('count desc')
+
+    @tags.each do |tag|
+      @entries.each do |entry|
+        tag.interactions ||= 0
+        tag.interactions += entry.total_count
+      end
+    end
+
+    @tags_count = {}
+    @tags.each { |n| @tags_count[n.name] = n.count }
+
+    @tags_interactions = {}
+    @entries.each do |entry|
+      entry.tags.each do |tag|
+        @tags_interactions[tag.name] ||= 0
+        @tags_interactions[tag.name] += entry.total_count
+      end
+    end
+
+    @tags_interactions = @tags_interactions.sort_by { |_k, v| v }
+    @tags_interactions.reverse
   end
 
   def check
