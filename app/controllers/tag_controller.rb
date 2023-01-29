8fb51a50ecf86e2631d8555b27ee1c21754a3c06
Bruno Sacco <brusacco@gmail.com>
2023-01-29 14:28:07 -0300

    added tag month filter

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -3,7 +3,7 @@
 class TagController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
-    @entries = Entry.joins(:site).tagged_with(@tag.name).has_image.order(published_at: :desc).limit(250)
+    @entries = Entry.a_month_ago.joins(:site).tagged_with(@tag.name).has_image.order(published_at: :desc)
     @tags = @entries.tag_counts_on(:tags).where.not(id: @tag.id).order('count desc').limit(20)
 
     # Sets counters and values
