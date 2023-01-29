be78f7e091e1293358c97625db18533e8ee89626
Bruno Sacco <brusacco@gmail.com>
2023-01-29 14:46:16 -0300

    added interactions tags to popular

--- app/controllers/entry_controller.rb
+++ app/controllers/entry_controller.rb
@@ -7,25 +7,22 @@ class EntryController < ApplicationController
     @entries = Entry.joins(:site).has_interactions.a_day_ago.where.not(image_url: nil).order(total_count: :desc)
     @tags = @entries.tag_counts_on(:tags).order('count desc')
 
+    # Sets counters and values
+    @tags_interactions = {}
     @tags.each do |tag|
       @entries.each do |entry|
         tag.interactions ||= 0
-        tag.interactions += entry.total_count
-      end
-    end
+        tag.interactions += entry.total_count if entry.tag_list.include?(tag.name)
 
-    @tags_count = {}
-    @tags.each { |n| @tags_count[n.name] = n.count }
-
-    @tags_interactions = {}
-    @entries.each do |entry|
-      entry.tags.each do |tag|
         @tags_interactions[tag.name] ||= 0
-        @tags_interactions[tag.name] += entry.total_count
+        @tags_interactions[tag.name] += entry.total_count if entry.tag_list.include?(tag.name)
       end
     end
 
     @tags_interactions = @tags_interactions.sort_by { |_k, v| v }
     @tags_interactions.reverse
+
+    @tags_count = {}
+    @tags.each { |n| @tags_count[n.name] = n.count }
   end
 end
