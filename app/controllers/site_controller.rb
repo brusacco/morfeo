80b67cbf7e013d8a34689362fe0dd2321e7030f6
Bruno Sacco <brusacco@gmail.com>
2023-02-22 19:25:53 -0300

    added more redis cache code

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -7,17 +7,24 @@ class SiteController < ApplicationController
     @entries = @site.entries.has_interactions.has_image.order(published_at: :desc).limit(250)
     @tags = @entries.tag_counts_on(:tags).order('count desc').limit(20)
 
-    # Sets counters and values
-    @tags_interactions = {}
-    @tags.each do |tag|
-      @entries.each do |entry|
-        tag.interactions ||= 0
-        tag.interactions += entry.total_count if entry.tag_list.include?(tag.name)
+     # Sets counters and values
+     @tags_interactions = Rails.cache.read("tags_interactions_#{@site.id}")
 
-        @tags_interactions[tag.name] ||= 0
-        @tags_interactions[tag.name] += entry.total_count if entry.tag_list.include?(tag.name)
-      end
-    end
+     # Cache tags interactions
+     if @tags_interactions.nil?
+       @tags_interactions = {}
+       @tags.each do |tag|
+         @entries.each do |entry|
+           tag.interactions ||= 0
+           tag.interactions += entry.total_count if entry.tag_list.include?(tag.name)
+ 
+           @tags_interactions[tag.name] ||= 0
+           @tags_interactions[tag.name] += entry.total_count if entry.tag_list.include?(tag.name)
+         end
+       end
+       Rails.cache.write("tags_interactions_#{@site.id}", @tags_interactions, expires_in: 1.hour)
+     end
+ 
 
     @tags_interactions = @tags_interactions.sort_by { |_k, v| v }
     @tags_interactions.reverse
