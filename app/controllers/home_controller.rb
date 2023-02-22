95777604e5e6e420a128932baaece353d3d3829d
Bruno Sacco <brusacco@gmail.com>
2023-02-22 19:21:06 -0300

    added redis cache to tags

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,15 +10,21 @@ class HomeController < ApplicationController
     @tags = @entries.tag_counts_on(:tags).order('count desc')
 
     # Sets counters and values
-    @tags_interactions = {}
-    @tags.each do |tag|
-      @entries.each do |entry|
-        tag.interactions ||= 0
-        tag.interactions += entry.total_count if entry.tag_list.include?(tag.name)
+    @tags_interactions = Rails.cache.read("tags_interactions")
 
-        @tags_interactions[tag.name] ||= 0
-        @tags_interactions[tag.name] += entry.total_count if entry.tag_list.include?(tag.name)
+    # Cache tags interactions
+    if @tags_interactions.nil?
+      @tags_interactions = {}
+      @tags.each do |tag|
+        @entries.each do |entry|
+          tag.interactions ||= 0
+          tag.interactions += entry.total_count if entry.tag_list.include?(tag.name)
+
+          @tags_interactions[tag.name] ||= 0
+          @tags_interactions[tag.name] += entry.total_count if entry.tag_list.include?(tag.name)
+        end
       end
+      Rails.cache.write("tags_interactions", @tags_interactions, expires_in: 1.hour)
     end
 
     @tags_interactions = @tags_interactions.sort_by { |_k, v| v }
