ce763d88f6591969800d5b13d443b1f7de9185a1
Bruno Sacco <brusacco@gmail.com>
2023-02-26 11:23:24 -0300

    update home list criteria

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,8 @@ class HomeController < ApplicationController
   def index
     @tag_interacions = []
     @sites = Site.where(total_count: 1..).order(total_count: :desc)
-    @entries = Entry.has_interactions.has_image.includes(:site).order(published_at: :desc).limit(300)
+    # @entries = Entry.has_interactions.has_image.includes(:site).order(published_at: :desc).limit(300)
+    @entries = Entry.has_image.includes(:site).order(published_at: :desc).limit(300)
     @tags = @entries.tag_counts_on(:tags).order('count desc')
 
     # Sets counters and values
