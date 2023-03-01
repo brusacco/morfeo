b2991669a06c1c122f8dacd7d61f8b12bb742176
Bruno Sacco <brusacco@gmail.com>
2023-02-28 23:34:15 -0300

    added bigrams to entry model and tags show

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -24,27 +24,44 @@ class Entry < ApplicationRecord
 
   def generate_bigrams
     # regex = /([A-ZÀ-Ö][a-zø-ÿ]{3,})\s([A-ZÀ-Ö][a-zø-ÿ]{3,})/
-    regex = /([a-zø-ÿ]{2,})\s([a-zø-ÿ]{2,})/
+    regex = /([a-zø-ÿ]{3,})\s([a-zø-ÿ]{3,})/
     bad_words = %w[Noticias Internacional Radio Noticiero Desde]
-    bad_words += %w[pero del de desde donde el los las la abc una un no mas por como que con para las fue más se su sus en al]
-    bad_words += File.readlines('stop-words.txt')
+    bad_words += %w[es pero del de desde donde el los las la abc una un no mas por como que con para las fue más se su sus en al]
+    bad_words += File.readlines('stop-words.txt').map { |a| a.strip }
+
+    
 
     bigrams = []
-    words = title.downcase.split + description.downcase.split
+    words = clean_text(title).split + clean_text(description).split + clean_text(content).split
     words.each_cons(2).each do |bigram|
       tag = bigram.join(' ')
       bigrams << tag if tag.match(regex) && !contains_substring?(tag, bad_words)
     end
-    bigrams
+    bigrams.uniq
   end
 
   private
+
+  def clean_text(text)
+    return '' if text.nil?
+    
+    text = text.gsub(/\d/, '')
+    text = text.gsub(/[[:punct:]]/, ' ')
+    text.downcase!
+    text.strip!
+    text
+  end
 
   def set_published_date
     self.published_date = published_at.to_date if published_at.present?
   end
 
   def contains_substring?(string, substrings)
-    substrings.any? { |substring| string.scan(substring).any? }
+    # substrings.any? { |substring| string.scan(substring).any? }
+    # substrings.any? { |substring| string.scan(/\b#{substrings}\b/).any? }
+    substrings.each do |substring|
+      return true if string.match(/\b#{substring}\b/)
+    end
+    return false
   end
 end
