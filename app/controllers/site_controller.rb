8adae26d0d004578213db8b8728e729da6b43863
Bruno Sacco <brusacco@gmail.com>
2023-03-02 12:12:05 -0300

    aa

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,7 +3,8 @@
 class SiteController < ApplicationController
   def show
     @site = Site.find(params[:id])
-    @entries = @site.entries.a_month_ago.order(published_at: :desc)
+    @entries_stats = @site.entries.a_month_ago.group_by_day(:published_at)
+    @entries = @site.entries.has_image.order(published_at: :desc).limit(250)
     @tags = @entries.tag_counts_on(:tags).order('count desc')
 
     # @bigrams = Rails.cache.read("bigrams_interactions_sites_#{@site.id}")
