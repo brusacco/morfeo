faf0cd25daf13089cb128c74de7e3871c033fbb3
Bruno Sacco <brusacco@gmail.com>
2023-03-02 11:45:13 -0300

    aa

--- app/controllers/entry_controller.rb
+++ app/controllers/entry_controller.rb
@@ -7,24 +7,16 @@ class EntryController < ApplicationController
     @entries = Entry.joins(:site).a_day_ago.where.not(image_url: nil).order(total_count: :desc)
     @tags = @entries.tag_counts_on(:tags).order('count desc')
 
-     # Sets counters and values
-     @tags_interactions = Rails.cache.read("tags_interactions_popular")
+    @tags_interactions = {}
+    @tags.each do |tag|
+      @entries.each do |entry|
+        tag.interactions ||= 0
+        tag.interactions += entry.total_count if entry.tag_list.include?(tag.name)
 
-     # Cache tags interactions
-     if @tags_interactions.nil?
-       @tags_interactions = {}
-       @tags.each do |tag|
-         @entries.each do |entry|
-           tag.interactions ||= 0
-           tag.interactions += entry.total_count if entry.tag_list.include?(tag.name)
- 
-           @tags_interactions[tag.name] ||= 0
-           @tags_interactions[tag.name] += entry.total_count if entry.tag_list.include?(tag.name)
-         end
-       end
-       Rails.cache.write("tags_interactions_popular", @tags_interactions, expires_in: 1.hour)
-     end
- 
+        @tags_interactions[tag.name] ||= 0
+        @tags_interactions[tag.name] += entry.total_count if entry.tag_list.include?(tag.name)
+      end
+    end
 
     @tags_interactions = @tags_interactions.sort_by { |_k, v| v }
     @tags_interactions.reverse
