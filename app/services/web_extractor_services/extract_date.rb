3fcf0c6c4d7ced4d0178abb6b056fe1b9394cfe0
Bruno Sacco <brusacco@gmail.com>
2023-03-02 22:53:05 -0300

    ss

--- app/services/web_extractor_services/extract_date.rb
+++ app/services/web_extractor_services/extract_date.rb
@@ -57,7 +57,7 @@ module WebExtractorServices
       elsif @doc.at('.fecha_detalle') && @date.nil?
         @date = @doc.at('.fecha_detalle').text
         @parsed = false
-      elsif @doc.at('.title-post li:nth-child(1)') & @date.nil?
+      elsif @doc.at('.title-post li:nth-child(1)') && @date.nil?
         @date = @doc.at('.title-post li:nth-child(1)').text
         @parsed = false
       else
