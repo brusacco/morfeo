a093154db49403a63e1cbc8d00a98a598e2ac5c6
Bruno Sacco <brusacco@gmail.com>
2023-03-17 14:29:44 -0300

    added bigram to tags show view

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -20,18 +20,31 @@ class TagController < ApplicationController
     @tags_interactions = {}
     @tags.each do |tag|
       @entries.each do |entry|
-        tag.interactions ||= 0
-        tag.interactions += entry.total_count if entry.tag_list.include?(tag.name)
+        if entry.tag_list.include?(tag.name)
+          tag.interactions ||= 0
+          tag.interactions += entry.total_count
 
-        @tags_interactions[tag.name] ||= 0
-        @tags_interactions[tag.name] += entry.total_count if entry.tag_list.include?(tag.name)
+          @tags_interactions[tag.name] ||= 0
+          @tags_interactions[tag.name] += entry.total_count
+        end
       end
     end
     
-    @tags_interactions = @tags_interactions.sort_by { |_k, v| v }
-    @tags_interactions.reverse
+    @tags_interactions = @tags_interactions.sort_by { |_k, v| v }.reverse
 
     @tags_count = {}
     @tags.each { |n| @tags_count[n.name] = n.count }
+
+    @bigrams = {}
+    @entries.each do |entry|
+      entry.bigrams.each do |bigram|
+        @bigrams[bigram] ||= 0
+        @bigrams[bigram] += 1
+      end
+    end
+    
+    @bigrams.delete_if { |_k, v| v < 2 }
+    @bigrams = @bigrams.sort_by { |_k, v| v }.reverse
+
   end
 end
