b13baedb19a94dba83c6ce55dad5dd9d18333e82
Bruno Sacco <brusacco@gmail.com>
2023-03-20 07:20:59 -0300

    fix

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -4,7 +4,7 @@ class TagController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
     @entries = Entry.normal_range.joins(:site).tagged_with(@tag.name).has_image.order(published_at: :desc)
-    @tags = @entries.tag_counts_on(:tags).where('count > 0').order('count desc').limit(20)
+    @tags = @entries.tag_counts_on(:tags).order('count desc').limit(20)
 
     @tags_interactions = {}
     @tags.each do |tag|
