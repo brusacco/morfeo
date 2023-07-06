2653d3aa6865b6ede72b37572fbf14ff432e9e37
Bruno Sacco <brusacco@gmail.com>
2023-07-06 16:39:26 -0400

    add tag notes

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -4,6 +4,17 @@ class TagController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
     @entries = Entry.normal_range.joins(:site).tagged_with(@tag.name).has_image.order(published_at: :desc)
+    @total_entries = @entries.size
+    @total_interactions = @entries.sum(:total_count)
+
+    if @total_entries.zero?
+      @promedio = 0
+    else
+      @promedio = @total_interactions / @total_entries
+    end
+  
+
+
     @tags = @entries.tag_counts_on(:tags).order('count desc').limit(20)
 
     @tags_interactions = {}
