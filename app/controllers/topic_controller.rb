9020fe8e05863b012c0c7bf80a77512bf439ba3a
Bruno Sacco <brusacco@gmail.com>
2023-08-03 11:01:04 -0400

    added more data to topics

--- app/controllers/topic_controller.rb
+++ app/controllers/topic_controller.rb
@@ -5,8 +5,11 @@ class TopicController < ApplicationController
     @topic = Topic.find(params[:id])
     @tag_list = @topic.tags.map(&:name)
     @entries = Entry.normal_range.joins(:site).tagged_with(@tag_list, any: true).has_image.order(published_at: :desc)
+
+    @top_entries = Entry.normal_range.joins(:site).order(total_count: :desc).limit(5)
+
     @total_entries = @entries.size
-    @total_interactions = @entries.sum(:total_count)
+    @total_interactions = @entries.sum(&:total_count)
 
     @most_interactions = @entries.sort_by(&:total_count).reverse.take(10)
 
