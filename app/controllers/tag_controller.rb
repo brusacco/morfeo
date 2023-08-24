d4532288b188da119932c39b9707f20579c94d64
Bruno Sacco <brusacco@gmail.com>
2023-08-24 08:45:30 -0400

    added word analisis

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -7,6 +7,9 @@ class TagController < ApplicationController
     @total_entries = @entries.size
     @total_interactions = @entries.sum(:total_count)
 
+    @word_occurrences = word_occurrences_from_entries(@entries)
+    @bigram_occurrences = bigram_occurrences_from_entries(@entries)
+
     @top_entries = Entry.normal_range.joins(:site).order(total_count: :desc).limit(5)
 
     @most_interactions = @entries.sort_by(&:total_count).reverse.take(8)
@@ -69,5 +72,39 @@ class TagController < ApplicationController
   def search
     query = params[:query].strip
     @tags = Tag.where('name LIKE?', "%#{query}%")
+  end
+
+  private
+
+  def bigram_occurrences_from_entries(entries)
+    word_occurrences = Hash.new(0)
+
+    entries.each do |entry|
+      words = (entry.title.to_s + ' ' + entry.content.to_s).gsub(/[[:punct:]]/, '').split
+      bigrams = words.each_cons(2).map { |word1, word2| "#{word1.downcase} #{word2.downcase}" }
+      bigrams.each do |bigram|
+        next if STOP_WORDS.include?(bigram.split.first) || STOP_WORDS.include?(bigram.split.last)
+
+        word_occurrences[bigram] += 1
+      end
+    end
+
+    word_occurrences.select { |_bigram, count| count > 10 }.sort_by { |_k, v| v }.reverse
+  end
+
+  def word_occurrences_from_entries(entries)
+    word_occurrences = Hash.new(0)
+
+    entries.each do |entry|
+      words = (entry.title.to_s + ' ' + entry.content.to_s).gsub(/[[:punct:]]/, '').split
+      words.each do |word|
+        cleaned_word = word.downcase
+        next if STOP_WORDS.include?(cleaned_word) || cleaned_word.length <= 1
+
+        word_occurrences[cleaned_word] += 1
+      end
+    end
+
+    word_occurrences.select { |_word, count| count > 10 }.sort_by { |_k, v| v }.reverse
   end
 end
