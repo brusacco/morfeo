b637ddd998210e367ec6b49c0b210a48554e00eb
Bruno Sacco <brusacco@gmail.com>
2023-08-28 13:49:04 -0400

    fix

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,2 +1,2 @@
 ENV["ELASTICSEARCH_URL"] = "http://localhost:9200" if Rails.env.development?
-ENV["ELASTICSEARCH_URL"] = "http://74.222.1.105/:9200" if Rails.env.production?
+ENV["ELASTICSEARCH_URL"] = "http://74.222.1.105:9200" if Rails.env.production?
