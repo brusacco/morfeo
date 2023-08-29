02ed56205eea2a59c379eb462a6ccd9a7a0b95ef
Bruno Sacco <brusacco@gmail.com>
2023-08-28 22:19:53 -0400

    remove model search

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,7 +4,6 @@ class Entry < ApplicationRecord
   acts_as_taggable_on :tags
   validates :url, uniqueness: true
   belongs_to :site, touch: true
-  searchkick
 
   scope :a_day_ago, -> { where(published_at: 1.day.ago..) }
   scope :a_week_ago, -> { where(published_at: 1.week.ago..) }
