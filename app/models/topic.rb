cac4673518c61569327ca95b01e707de0d8528a9
Bruno Sacco <brusacco@gmail.com>
2023-09-11 11:47:44 -0400

    trim spaces

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,4 +5,13 @@ class Topic < ApplicationRecord
   has_many :topic_words, dependent: :destroy
   has_and_belongs_to_many :tags
   accepts_nested_attributes_for :tags
+
+  before_update :remove_words_spaces
+
+  private
+
+  def remove_words_spaces
+    self.positive_words = positive_words.gsub(' ', '')
+    self.negative_words = negative_words.gsub(' ', '')
+  end
 end
