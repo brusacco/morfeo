8b2027326ef119c841d56b67ac3b638ca35488f0
Bruno Sacco <brusacco@gmail.com>
2023-09-18 11:57:51 -0400

    update cron file

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,9 +6,12 @@ every :hour do
   rake 'crawler'
   rake 'update_stats'
   rake 'update_site_stats'
+  rake 'facebook:fanpage_crawler'
+  rake 'facebook:comment_crawler'
 end
 
 every 6.hours do
   rake 'crawler_deep'
   rake 'generate_ai_reports'
+  rake 'facebook:update_fanpages'
 end
