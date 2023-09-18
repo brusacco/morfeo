b7dde7481f94d786176fcb9cbfe1486230a059b8
Bruno Sacco <brusacco@gmail.com>
2023-09-18 12:08:43 -0400

    fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,6 +6,9 @@ every :hour do
   rake 'crawler'
   rake 'update_stats'
   rake 'update_site_stats'
+end
+
+every 2.hours do
   rake 'facebook:fanpage_crawler'
   rake 'facebook:comment_crawler'
 end
