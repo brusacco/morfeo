8fe701bb74867cbb4f975c84d7e42cb9f85afca4
Bruno Sacco <brusacco@gmail.com>
2023-10-09 10:17:48 -0300

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,7 +41,7 @@ module ApplicationHelper
     # raise ArgumentError, 'max_value must be greater than min_value' if max_value <= min_value
 
     # Calculate the normalized value on a scale from 1 to 10
-    normalized = (((value - min_value) * 9) / (max_value - min_value)) + 1
+    normalized = (((value - min_value) * 9) / ((max_value - min_value)) + 1)
 
     # Ensure the result is within the range [1, 10]
     normalized.clamp(1, 10)
