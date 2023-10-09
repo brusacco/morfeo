230ad657888b558077ac9e400afebcd62dc7b83f
Bruno Sacco <brusacco@gmail.com>
2023-10-09 10:15:52 -0300

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,7 +38,7 @@ module ApplicationHelper
 
   def normalize_to_scale(value, max_value, min_value)
     # Ensure that max_value is greater than min_value to avoid division by zero
-    raise ArgumentError, 'max_value must be greater than min_value' if max_value <= min_value
+    # raise ArgumentError, 'max_value must be greater than min_value' if max_value <= min_value
 
     # Calculate the normalized value on a scale from 1 to 10
     normalized = (((value - min_value) * 9) / (max_value - min_value)) + 1
