c7ea79fad6536f26fb497d4432fa20fc5050faad
Bruno Sacco <brusacco@gmail.com>
2023-10-10 15:10:08 -0300

    add touch to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
-  belongs_to :entry
+  belongs_to :entry, touch: true
 
   def self.word_occurrences(limit = 100)
     word_occurrences = Hash.new(0)
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Report < ApplicationRecord
-  belongs_to :topic
+  belongs_to :topic, touch: true
 end
