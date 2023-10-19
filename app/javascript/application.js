29ee52cf1495be62eece8b39f545007ce97ec589
Fingolfin <v.pedromartinez@gmail.com>
2023-10-19 12:32:14 -0300

    Remove Topics Dropdown

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,7 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "chartkick"
-import 'flowbite';
+// import 'flowbite';
 
 // document.addEventListener('turbo:load', function () {
 //   console.log("hola");
