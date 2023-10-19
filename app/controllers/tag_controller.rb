a7b207fabd6435d79ce4bb41fa06b37fc28c79fd
Fingolfin <v.pedromartinez@gmail.com>
2023-10-19 17:38:25 -0300

    Errorres comentados en Tags

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -24,9 +24,10 @@ class TagController < ApplicationController
     @negatives = @entries.where(polarity: 2).count('*')
     @neutrals = @entries.where(polarity: 0).count('*')
 
-    @percentage_positives = (@positives.to_f / @entries.size * 100).round(0)
-    @percentage_negatives = (@negatives.to_f / @entries.size * 100).round(0)
-    @percentage_neutrals = (@neutrals.to_f / @entries.size * 100).round(0)
+    # Comentado de momento para evitar errores
+    # @percentage_positives = (@positives.to_f / @entries.size * 100).round(0)
+    # @percentage_negatives = (@negatives.to_f / @entries.size * 100).round(0)
+    # @percentage_neutrals = (@neutrals.to_f / @entries.size * 100).round(0)
 
     @top_entries = Entry.normal_range.joins(:site).order(total_count: :desc).limit(5)
     @most_interactions = @entries.sort_by(&:total_count).reverse.take(8)
