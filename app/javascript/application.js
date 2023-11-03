7ff011740218b85662882121920917b9aec558f4
Fingolfin <v.pedromartinez@gmail.com>
2023-11-03 11:16:23 -0300

    Logout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,6 +4,23 @@ import "controllers"
 import "chartkick"
 import 'flowbite';
 
-// document.addEventListener('turbo:load', function () {
-//   console.log("hola");
-// });
+document.addEventListener('turbo:load', function () {
+  // profile - logout
+  const userMenuButton = document.getElementById('user-menu-button');
+  const userMenuDropdown = document.querySelector('.absolute.right-0');
+
+  let isMenuOpen = false;
+
+  userMenuButton.addEventListener('click', () => {
+    isMenuOpen = !isMenuOpen;
+    userMenuDropdown.classList.toggle('hidden', !isMenuOpen);
+  });
+
+  // Cerrar el menú si se hace clic en cualquier otro lugar de la página
+  window.addEventListener('click', (event) => {
+    if (isMenuOpen && !userMenuButton.contains(event.target)) {
+      isMenuOpen = false;
+      userMenuDropdown.classList.add('hidden');
+    }
+  });
+});
