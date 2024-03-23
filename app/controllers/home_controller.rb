67fc99724d1cecd7a048847bb9aff416a169aa28
Bruno Sacco <brusacco@gmail.com>
2024-03-23 19:22:21 -0300

    Add caching for HomeController#index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  caches_action :index, expires_in: 1.hour
   before_action :authenticate_user!, except: :deploy
   skip_before_action :verify_authenticity_token
 
