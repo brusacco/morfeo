280f47f40a880f50243a3b6e6e84f87e6fe17188
Bruno Sacco <brusacco@gmail.com>
2024-07-14 13:54:07 -0400

    Refactor home/index.html.erb to limit the number of entries per topic to 12

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,30 +6,6 @@ class HomeController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @sites = Site.where(total_count: 1..).order(total_count: :desc)
-    @entries = Entry.includes(:site).order(published_at: :desc).limit(100)
-    @tags = @entries.tag_counts_on(:tags).order('count desc')
-
-    @tags = @tags.select { |tag| tag.count > 1 }
-
-    @tags_interactions = {}
-    @tags.each do |tag|
-      @entries.each do |entry|
-        tag.interactions ||= 0
-        tag.interactions += entry.total_count if entry.tag_list.include?(tag.name)
-
-        @tags_interactions[tag.name] ||= 0
-        @tags_interactions[tag.name] += entry.total_count if entry.tag_list.include?(tag.name)
-      end
-    end
-
-    @tags_interactions = @tags_interactions.select { |_k, v| v > 1 }
-    @tags_interactions = @tags_interactions.sort_by { |_k, v| v }
-    @tags_interactions.reverse
-
-    @tags_count = {}
-    @tags.each { |n| @tags_count[n.name] = n.count }
-
     # Chart Multiple Interacciones x DIA x TOPICO
     topics = current_user.topics.where(status: true)
     @interacciones_dia_topico = topics.map do |topic|
