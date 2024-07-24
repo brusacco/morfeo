c81fe7e5c7ede3fbc32e5f843dd544bbe94fa42c
Fingolfin <v.pedromartinez@gmail.com>
2024-07-24 10:35:11 -0400

    Debug Papertrail

---
+++ app/admin/versions.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Version do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :item_type, :item_id, :event, :whodunnit, :object
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:item_type, :item_id, :event, :whodunnit, :object]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
