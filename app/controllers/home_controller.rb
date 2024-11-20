da36574018c072adc5ce550939d61f9580453099
Pedro Martinez <v.pedromartinez@gmail.com>
2024-11-20 10:47:57 -0300

    Add user agent to Nokogiri on check method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -153,7 +153,7 @@ class HomeController < ApplicationController
 
   def check
     @url = params[:url]
-    @doc = Nokogiri::HTML(URI.parse(@url).open)
+    @doc = Nokogiri::HTML(URI.parse(@url).open("User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"))
     @result = WebExtractorServices::ExtractDate.call(@doc)
     render layout: false
   end
