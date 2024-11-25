f49785c18ccc83a56b28602055da5363b5ceca82
Bruno Sacco <brusacco@gmail.com>
2024-11-25 09:14:58 -0300

    Fix selector for date extraction in WebExtractorServices

--- app/services/web_extractor_services/extract_date.rb
+++ app/services/web_extractor_services/extract_date.rb
@@ -78,8 +78,8 @@ module WebExtractorServices
       elsif @doc.at('.main_content h4') && @date.nil?
         @date = @doc.at('.main_content h4').text
         @parsed = false
-      elsif @doc.at('c-hero__text--date') && @date.nil?
-        @date = @doc.at('c-hero__text--date').text
+      elsif @doc.at('.c-hero__text--date') && @date.nil?
+        @date = @doc.at('.c-hero__text--date').text
         @parsed = false
       else
         @date = nil
