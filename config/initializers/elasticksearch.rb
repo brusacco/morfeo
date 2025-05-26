a6e38f5dafc9979b997aa20032f9333e2b155c83
Pedro Martinez <v.pedromartinez@gmail.com>
2025-05-26 17:30:15 -0300

    Update elasticksearch connection

--- config/initializers/elasticksearch.rb
+++ config/initializers/elasticksearch.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env.production?
-  ENV['ELASTICSEARCH_URL'] = "http://elastic:7TXB9YP5h6aJn8KrGA8V@www.morfeo.com.py:9200"
+  ENV['ELASTICSEARCH_URL'] = "http://elastic:7TXB9YP5h6aJn8KrGA8V@localhost:9200"
 elsif Rails.env.development?
   ENV['ELASTICSEARCH_URL'] = 'http://localhost:9200'
 end
