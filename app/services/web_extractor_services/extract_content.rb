3dd0744c7d9e741e94d787c69967f5c07c5b9644
Bruno Sacco <brusacco@gmail.com>
2025-06-12 21:56:10 -0300

    aaa

--- app/services/web_extractor_services/extract_content.rb
+++ app/services/web_extractor_services/extract_content.rb
@@ -14,7 +14,6 @@ module WebExtractorServices
         @doc.css('script').remove
         @doc.css('style').remove
         @doc.css('a').remove
-        @doc.css('b').remove
         sanitized_string = @doc.at(@content_filter).text.strip
         sanitized_string = sanitized_string.gsub(/[\n\r\t]/, '').squish
         result = { content: sanitized_string }
