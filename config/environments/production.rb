d6b0ebd0abfeb8c58eafe9b35f2d2fbfc5d8179f
Bruno Sacco <brusacco@gmail.com>
2025-09-13 11:05:36 -0300

    aaa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  config.hosts << 'localhost:7500'
   config.hosts << 'morfeo.com.py'
   # Settings specified here will take precedence over those in config/application.rb.
 
