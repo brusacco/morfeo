0746c9a65d921db2df7ec9940600934e93cf3fe5
Bruno Sacco <brusacco@gmail.com>
2025-10-15 23:50:15 -0300

    Refactor index creation for taggings to improve readability and maintainability

--- db/migrate/20251016024900_add_index_taggings_on_tag_id_and_taggable_type.rb
+++ db/migrate/20251016024900_add_index_taggings_on_tag_id_and_taggable_type.rb
@@ -1,5 +1,7 @@
 class AddIndexTaggingsOnTagIdAndTaggableType < ActiveRecord::Migration[7.0]
   def change
-    add_index :taggings, [:tag_id, :taggable_type], name: 'index_taggings_on_tag_id_and_taggable_type' unless index_exists?(:taggings, [:tag_id, :taggable_type])
+    return if index_exists?(:taggings, %i[tag_id taggable_type])
+
+    add_index :taggings, %i[tag_id taggable_type], name: 'index_taggings_on_tag_id_and_taggable_type'
   end
 end
