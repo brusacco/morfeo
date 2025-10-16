bf46270ba674e221bd28c632e4d847715a2ed69c
Bruno Sacco <brusacco@gmail.com>
2025-10-16 02:04:00 -0300

    Refactor word cloud data preparation in ApplicationHelper, TagHelper, and TopicHelper for improved readability and reusability

--- app/helpers/tag_helper.rb
+++ app/helpers/tag_helper.rb
@@ -9,4 +9,18 @@ module TagHelper
 
     data.transform_values { |count| (count / total * 100).round(0) }
   end
+
+  def prepare_word_cloud_data(word_occurrences, positive_words, negative_words)
+    return [] if word_occurrences.blank?
+
+    min_max = find_max_and_min_occurrences(word_occurrences)
+
+    word_occurrences.shuffle { |a, b| a[1] <=> b[1] }.map do |word, value|
+      {
+        word: word,
+        color: word_color(positive_words, negative_words, word),
+        weight: normalize_to_scale(value, min_max[:max], min_max[:min])
+      }
+    end
+  end
 end
--- app/helpers/topic_helper.rb
+++ app/helpers/topic_helper.rb
@@ -14,4 +14,18 @@ module TopicHelper
 
     (topic_value / total * 100).round(1)
   end
+
+  def prepare_word_cloud_data(word_occurrences, positive_words, negative_words)
+    return [] if word_occurrences.blank?
+
+    min_max = find_max_and_min_occurrences(word_occurrences)
+
+    word_occurrences.shuffle { |a, b| a[1] <=> b[1] }.map do |word, value|
+      {
+        word: word,
+        color: word_color(positive_words, negative_words, word),
+        weight: normalize_to_scale(value, min_max[:max], min_max[:min])
+      }
+    end
+  end
 end
