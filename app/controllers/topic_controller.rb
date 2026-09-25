0c094817fa30bfae60da99a420ecb79e937aaf0a
Bruno Sacco <brusacco@gmail.com>
2026-09-25 20:21:52 -0300

    aaaa

--- app/controllers/topic_controller.rb
+++ app/controllers/topic_controller.rb
@@ -7,8 +7,12 @@ class TopicController < ApplicationController
   before_action :set_topic, only: %i[show pdf comments history]
   before_action :authorize_topic_access!, only: %i[show pdf]
 
-  caches_action :show, :pdf, expires_in: 30.minutes,
-                cache_path: proc { |c| { topic_id: c.params[:id], user_id: c.current_user.id, days_range: c.params[:days_range] } }
+  caches_action :show,
+                :pdf,
+                expires_in: 30.minutes,
+                cache_path: proc { |c|
+                  { topic_id: c.params[:id], user_id: c.current_user.id, days_range: c.params[:days_range] }
+                }
 
   def entries_data
     topic_id = params[:topic_id]
